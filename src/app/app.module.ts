25e6ca67043b22c3518f35db9515256bf76160f0
V Kuznetsov <vvsmith13@gmail.com>
2025-03-25 14:53:17 +0300

    1.4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
+import { ItemComponent } from './item/item.component';
+import { DetailsComponent } from './item/details/details.component';
+import { ListComponent } from './item/list/list.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactComponent,
+    AboutComponent,
+    ItemComponent,
+    DetailsComponent,
+    ListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/item/details/details.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-details',
+  templateUrl: './details.component.html',
+  styleUrls: ['./details.component.css'],
+})
+export class DetailsComponent implements OnInit {
+  ngOnInit(): void {
+    console.log('55555');
+  }
+}
