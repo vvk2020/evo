32107f7c091fc64065f0e5e236a56ef62d4ccc94
V Kuznetsov <vvsmith13@gmail.com>
2025-03-30 23:34:21 +0300

    1.5.5

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainComponent } from './main/main.component';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 
 const routes: Routes = [
   {
     path: '',
+    component: AppComponent,
+  },
+  {
+    path: 'main',
     component: MainComponent,
   },
   {
