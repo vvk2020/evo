388ea72583c4cb0ab3af7a315e8b4904effd016a
V Kuznetsov <vvsmith13@gmail.com>
2025-03-31 07:58:56 +0300

    1.5.8

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatTabsModule } from '@angular/material/tabs';
 
+import { MatMenuModule } from '@angular/material/menu';
+
 @NgModule({
-  declarations: [
-    AppComponent,
-    MainComponent
-  ],
+  declarations: [AppComponent, MainComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatTabsModule
+    MatTabsModule,
+    MatMenuModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
