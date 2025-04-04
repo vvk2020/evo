dc1788e07982484f724d112b9d01c4378dd7edee
V Kuznetsov <vvsmith13@gmail.com>
2025-04-04 19:39:47 +0300

    1.7.1,3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'evoApp';
+export class AppComponent implements OnInit {
+  // title = 'evoApp';
+  constructor(public title: Title) {}
+
+  ngOnInit(): void {
+    this.title.setTitle('Desk Page');
+  }
 }
