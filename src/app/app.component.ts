7700104c75cc9b9b432247a6281e17ae1f70e44e
V Kuznetsov <vvsmith13@gmail.com>
2025-04-23 12:38:09 +0300

    2.6.5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,29 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ComponentRef,
+  ViewChild,
+  ViewContainerRef,
+} from '@angular/core';
+import { DynamicComponent } from './dynamic/dynamic.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {}
+export class AppComponent {
+  @ViewChild('dynamicComp', { read: ViewContainerRef })
+  private viewRef!: ViewContainerRef;
+  private componentRef!: ComponentRef<DynamicComponent>;
+
+  // Динамическое добавление компонента DynamicComponent
+  addDynamicComp() {
+    this.viewRef.clear();
+    this.componentRef = this.viewRef.createComponent(DynamicComponent);
+  }
+
+  // Динамическое удаление компонента DynamicComponent
+  delDynamicComp() {
+    this.viewRef.clear();
+  }
+}
