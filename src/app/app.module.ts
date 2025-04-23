4caf9fbf9725d9de17b91e6922404a6c7dfb828e
V Kuznetsov <vvsmith13@gmail.com>
2025-04-23 14:31:44 +0300

    2.6

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,19 +7,18 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
+import { MatTableModule } from '@angular/material/table';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    DynamicComponent
-  ],
+  declarations: [AppComponent, DynamicComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatButtonModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatTableModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/dynamic/dynamic.component.ts
+++ src/app/dynamic/dynamic.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-dynamic',
   templateUrl: './dynamic.component.html',
-  styleUrls: ['./dynamic.component.css']
+  styleUrls: ['./dynamic.component.css'],
 })
 export class DynamicComponent {
+  displayedColumns: string[] = ['id', 'name'];
+  users = [
+    { id: 1, name: 'Сплин' },
+    { id: 2, name: 'Кино' },
+    { id: 3, name: 'Машина времени' },
+    { id: 4, name: 'Би-2' },
+    { id: 5, name: 'ДДТ' },
+    { id: 6, name: 'Аквариум' },
+    { id: 7, name: 'Nautilus Pompilius' },
+    { id: 8, name: 'Ночные снайперы' },
+    { id: 9, name: 'Пикник' },
+    { id: 10, name: 'Чайф' },
+  ];
 
+  data = new MatTableDataSource(this.users);
 }
