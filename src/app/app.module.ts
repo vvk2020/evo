b29bb8fb8a638498a608aa4ae0a6b6da636fc83f
V Kuznetsov <vvsmith13@gmail.com>
2025-04-23 12:02:48 +0300

    2.6.2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { DynamicComponent } from './dynamic/dynamic.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { DynamicComponent } from './dynamic/dynamic.component';
     DynamicComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
