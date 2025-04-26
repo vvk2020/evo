26b7f526bb373019215f1d4908138cb00f56ea0a
V Kuznetsov <vvsmith13@gmail.com>
2025-04-26 07:53:18 +0300

    2.8.3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,22 +18,55 @@ export class AppComponent implements OnInit {
 
   ngOnInit() {
     this.todoForm = this.fb.group({
-      textTodoInput: ['', Validators.required, Validators.pattern(/^[*]{3,200}$/)], // можно добавать валидаторы
+      textTodoInput: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(3),
+          Validators.maxLength(200),
+        ],
+      ],
     });
   }
 
-  public onAddTodo() {
-    if (this.todoForm.invalid) return;
-    this.store.dispatch(
-      new AddTodo({
-        text: this.todoForm.value.textTodoInput,
-        status: true,
-      })
-    );
-    this.todoForm.reset();
+  public onSubmit() {
+    if (this.todoForm.valid) {
+      this.store.dispatch(
+        new AddTodo({
+          text: this.todoForm.value.textTodoInput,
+          status: true,
+        })
+      );
+      this.onReset();
+    } else {
+      this.todoForm.markAllAsTouched();
+    }
   }
 
   public onClearTodoList() {
     this.store.dispatch(new ClearTodosList());
+  }
+
+  // Сброс формы к начальному (валидному) состоянию
+  public onReset(): void {
+    // Сбрас значений полей к "" (вместо null)
+    this.todoForm.reset({
+      title: '',
+      author: '',
+    });
+
+    // Сброс состояния формы
+    this.todoForm.markAsPristine(); // нет изменений (dirty=false)
+    this.todoForm.markAsUntouched(); // нет взаимодействия (touched=false)
+
+    // Очиcтка ошибок валидации
+    Object.keys(this.todoForm.controls).forEach((key) => {
+      this.todoForm.get(key)?.setErrors(null);
+    });
+
+    // Принудительное обновление (для Angular Material)
+    setTimeout(() => {
+      this.todoForm.updateValueAndValidity();
+    });
   }
 }
