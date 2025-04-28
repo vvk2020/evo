6896c16f08b115cf783f747a11d9784cb352f39c
V Kuznetsov <vvsmith13@gmail.com>
2025-04-28 14:12:46 +0300

    1.10.1

---
+++ workspace/projects/table/src/lib/table.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-table',
+  template: `
+    <p>
+      table works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class TableComponent {
+
+}
